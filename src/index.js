862bbf4f7b4c8afd20245fdacba906201aab4f15
LaziukAlena1988 <miledyhel@mail.ru>
2026-01-22 23:26:21 +0300

    выполнена валидация форм и калькулятора

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer.js";
 import menu from "./modules/menu.js";
 import modal from "./modules/modal.js";
+import validate from "./modules/validate.js";
 
 timer("31 january 2026");
 menu();
 modal();
+validate();
---
+++ src/modules/validate.js
@@ -0,0 +1,39 @@
+const validate = () => {
+  const calcInputs = document.querySelectorAll(
+    ".calc-square, .calc-count, .calc-day",
+  );
+
+  calcInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      input.value = input.value.replace(/[^\d]/g, "");
+    });
+  });
+
+  const textInputs = document.querySelectorAll(
+    'input[type="text"]:not(.calc-square):not(.calc-count):not(.calc-day), input[placeholder="Ваше сообщение"]',
+  );
+
+  textInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      input.value = input.value.replace(/[^а-яёА-ЯЁ\s-]/g, "");
+    });
+  });
+
+  const emailInputs = document.querySelectorAll('input[type="email"]');
+
+  emailInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      input.value = input.value.replace(/[^a-zA-Z0-9@\-_.!~*']/g, "");
+    });
+  });
+
+  const phoneInputs = document.querySelectorAll('input[type="tel"]');
+
+  phoneInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      input.value = input.value.replace(/[^0-9()-]/g, "");
+    });
+  });
+};
+
+export default validate;
