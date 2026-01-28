86b0566a9fda148af50b7c8cdcafeca2ffb2d1f2
LaziukAlena1988 <miledyhel@mail.ru>
2026-01-28 22:12:38 +0300

    модуль слайдер

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu.js";
 import modal from "./modules/modal.js";
 import validate from "./modules/validate.js";
 import tabs from "./modules/tabs.js";
+import slider from "./modules/slider.js";
 
 timer("31 january 2026");
 menu();
 modal();
 validate();
 tabs();
+slider();
